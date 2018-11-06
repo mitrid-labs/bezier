acd29c1df58a6fafccb9289254ef29751be1da4b
Christian Nyumbayire <chritchens@gmail.com>
2018-11-06 23:58:41 +0100

    Add the crypto module

---
+++ src/crypto/mod.rs
@@ -0,0 +1,7 @@
+pub mod hash;
+pub mod sign;
+pub mod prove;
+
+pub use self::hash::*;
+pub use self::sign::*;
+pub use self::prove::*;
---
+++ src/main.rs
@@ -0,0 +1,11 @@
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate sodiumoxide;
+extern crate mitrid_core;
+
+mod crypto;
+
+fn main() {
+    println!("Hello, world!");
+}
